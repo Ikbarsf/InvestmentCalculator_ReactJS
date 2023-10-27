2ce136821b35d2a8e9806db26d01f45026af3ae9
Ikbarsf <ikbarsaif@gmail.com>
2023-10-27 21:50:42 +0700

    create template with reactJS

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,45 @@
+function Calculator({ onChangeProps, userInput }) {
+  return (
+    <div id="user-input" action="">
+      <div className="input-group">
+        <p>
+          <label htmlFor="">Inisial Investasi</label>
+          <input
+            type="number"
+            value={userInput.initialInvestment}
+            onChange={(e) => onChangeProps("initialInvestment", e.target.value)}
+          />
+        </p>
+        <p>
+          <label htmlFor="">Inisial Tahunan</label>
+          <input
+            type="number"
+            value={userInput.annualInvestment}
+            onChange={(e) => onChangeProps("annualInvestment", e.target.value)}
+          />
+        </p>
+      </div>
+
+      <div className="input-group">
+        <p>
+          <label htmlFor="">Perkiraan Balik Modal</label>
+          <input
+            type="number"
+            value={userInput.expectedReturn}
+            onChange={(e) => onChangeProps("expectedReturn", e.target.value)}
+          />
+        </p>
+        <p>
+          <label htmlFor="">Durasi</label>
+          <input
+            type="number"
+            value={userInput.duration}
+            onChange={(e) => onChangeProps("duration", e.target.value)}
+          />
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Calculator;
